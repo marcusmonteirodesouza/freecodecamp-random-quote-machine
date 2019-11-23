b8f31918f748356312fddcc2ea495340da0ddf18
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2019-11-23 23:18:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Random Quote Machine
+
+My solution to [FreeCodeCamp's Random Quote Machine challenge](https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-random-quote-machine)
---
+++ src/components/App/app.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {mount} from 'enzyme'
+import App from '.'
+
+describe('<App />', () => {
+  it('renders', () => {
+    const wrapper = mount(<App />)
+    expect(wrapper.text()).toBe('Hello, World!')
+  })
+})
