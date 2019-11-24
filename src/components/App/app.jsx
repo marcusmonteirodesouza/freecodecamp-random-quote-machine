d74bb5f6bc61850853c12944a507ca72c4f69f84
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2019-11-24 22:57:50 +0100

    Start styling

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import QuoteBox from '../Quotes/QuoteBox';
 
+const styles = {
+  app: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  quoteBoxContainer: {}
+};
+
 const App = () => {
   return (
-    <div>
-      <QuoteBox />
+    <div style={styles.app}>
+      <div style={styles.quoteBoxContainer}>
+        <QuoteBox />
+      </div>
     </div>
   );
 };
--- src/components/Quotes/QuoteBox/quote-box.jsx
+++ src/components/Quotes/QuoteBox/quote-box.jsx
@@ -25,11 +25,12 @@ const QuoteBox = ({ quoteService }) => {
   return quoteData ? (
     <div id="quote-box" data-testid="quote-box">
       <blockquote>
-        <p id="text">
-          - <span>{quoteData.quote}</span>
-        </p>
+        <p id="text">{`"${quoteData.quote}"`}</p>
         <footer>
-          <p id="author">{quoteData.author}</p>
+          <p
+            style={{ textAlign: 'right' }}
+            id="author"
+          >{`- ${quoteData.author}`}</p>
           <cite>
             <a
               href={quoteData.authorPage}
@@ -41,22 +42,27 @@ const QuoteBox = ({ quoteService }) => {
           </cite>
         </footer>
       </blockquote>
-      <button
-        type="button"
-        id="new-quote"
-        onClick={async () => setQuoteData(await quoteService.fetchQuoteData())}
-      >
-        new quote
-      </button>
-      <a
-        id="tweet-quote"
-        className="twitter-share-button"
-        href={`https://twitter.com/intent/tweet?text=${quoteData.quote} ${quoteData.author}&hashtags=quotes`}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FontAwesomeIcon icon={faTwitter} />
-      </a>
+      <div style={{ textAlign: 'center' }}>
+        <button
+          type="button"
+          id="new-quote"
+          onClick={async () =>
+            setQuoteData(await quoteService.fetchQuoteData())
+          }
+        >
+          new quote
+        </button>
+        <a
+          style={{ marginLeft: '.5em' }}
+          id="tweet-quote"
+          className="twitter-share-button"
+          href={`https://twitter.com/intent/tweet?text=${quoteData.quote} ${quoteData.author}&hashtags=quotes`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+      </div>
     </div>
   ) : (
     <p>Loading quote...</p>
